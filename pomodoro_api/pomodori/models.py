2c3a1376a16fb0e03508fc72f678c608ae4f9f6e
Thibault Jouannic <thibault@miximum.fr>
2016-03-24 10:40:40 +0100

    Create pomodoro model

--- pomodoro_api/pomodori/models.py
+++ pomodoro_api/pomodori/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Pomodoro(models.Model):
+    author = models.ForeignKey('accounts.User')
+    started_at = models.DateTimeField(default=timezone.now)
+    ended_at = models.DateTimeField(null=True, blank=True)
+    interrupted = models.BooleanField(default=False)
